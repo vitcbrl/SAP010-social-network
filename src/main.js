a6fdcd7530b9d8c5dd074e3ae75a797db9a9feac
Larissa Korz <lariskorz@gmail.com>
2023-05-29 11:19:33 -0300

    add pasta img

--- src/main.js
+++ src/main.js
@@ -1,35 +1,29 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 import sobre from './pages/sobre/index.js';
 import inicio from './pages/inicio/index.js';
 
-const main = document.querySelector("#root");
+const main = document.querySelector('#root');
 
 const init = () => {
-    window.addEventListener("hashchange", () => {
-        main.innerHTML=""
-        switch(window.location.hash){
-            case "":
-                main.appendChild(inicio());
-                break;
-            case "#sobre":
-                main.appendChild(sobre());
-                break;
-            default:
-                main.appendChild(inicio());
-        }
-    });
-}
+  window.addEventListener('hashchange', () => {
+    main.innerHTML = '';
+    switch (window.location.hash) {
+      case '':
+        main.appendChild(inicio());
+        break;
+      case '#sobre':
+        main.appendChild(sobre());
+        break;
+      default:
+        main.appendChild(inicio());
+    }
+  });
+};
 
-window.addEventListener("load", () => {
-    main.appendChild(inicio());
-    init();
+window.addEventListener('load', () => {
+  main.appendChild(inicio());
+  init();
 });
-
-
-
-
-
-
-myFunction();
+// myFunction();
