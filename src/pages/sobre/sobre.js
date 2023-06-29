b065b0de7eed4585d8b7628e98aceff21f715ede
Raquel Maia <raquelmaia860@gmail.com>
2023-06-29 09:18:04 -0300

    atualizando caminho das imagens

--- src/pages/sobre/sobre.js
+++ src/pages/sobre/sobre.js
@@ -16,21 +16,21 @@ Por isso, criamos um espaço seguro onde vocês podem compartilhar, se expressar
     <section class='sobre-dev'>
     <h1 class='title-sobre'>Conheça as mulheres que Desenvolveram a Techsisters:</h1>
         <div class='sobre-raq'>
-            <img src='img/img-raq.png' alt='Raquel Maia' class='foto'/>
+            <img src='/img-raq.png' alt='Raquel Maia' class='foto'/>
             <h2 class='h2_sobre'>Raquel Maia</h2>
             <p class='p_sobre'>
             Hello, World! Sou a Raquel Maia, uma nordestina de Fortaleza-CE apaixonada por tecnologia, mas atualmente morando em São Paulo. Estou cursando Análise e Desenvolvimento de Sistemas e participando do Bootcamp da Laboratória para aprimorar minhas habilidades em HTML, CSS, JavaScript e outras tecnologias. Com minha combinação única de conhecimentos em marketing e programação, tenho a capacidade de oferecer soluções inovadoras. Estou entusiasmada em crescer como desenvolvedora de sistemas e contribuir para a evolução da tecnologia!
             </p>
         </div>
         <div class='sobre-lari'>
-            <img src='img/larissa.jpeg' alt='Larissa Korz' class='foto'/>
+            <img src='/larissa.jpeg' alt='Larissa Korz' class='foto'/>
             <h2 class='h2_sobre'>Larissa Korz</h2>
             <p class='p_sobre'>
                 Hi!! Sou Larissa Korz, natural de Santa Catarina. Estou cursando Análise e Desenvolvimento de Sistemas, Processos Gerenciais e o Bootcamp da Laboratória, onde tenho a oportunidade de aprimorar meus conhecimentos teóricos, também desenvolvendo minhas habilidades técnicas e aprendendo a trabalhar em equipe. Em 2022 decidi sair do meu emprego para me dedicar exclusivamente ao aprimoramento profissional. Busco aplicar minhas habilidades de programação em projetos desafiadores e inovadores, colaborando com uma equipe talentosa e fazendo a diferença na vida das pessoas por meio da tecnologia. <br>
             </p>
         </div>
         <div class='sobre-vi'>
-            <img src='img/vi.jpeg' alt='Vitoria Cabral' class='foto'/>
+            <img src='/vi.jpeg' alt='Vitoria Cabral' class='foto'/>
             <h2 class='h2_sobre'>Vitoria Cabral</h2>
             <p class='p_sobre'>
                 Hello, World! Eu sou a Vitoria Cabral, com raízes em São Paulo mas atualmente morando em Fortaleza-CE. Estou cursando Análise e Desenvolvimento de Sistemas, além disso, também estou no BootCamp da Laboratória me aprofundando em HTML, CSS, JavaScript e outras tecnologias. Busco ampliar meu horizonte de conhecimento e no processo ajudar outras mulheres que estão seguindo o mesmo caminho, sendo sempre proativa, empatica e com foco nos meus objetivos. <br>
